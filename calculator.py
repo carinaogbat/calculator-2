01144b62b322fb113dab62fb86b73fd0b2c5fd98
Carina Ogbat <carinawrightogbat@gmail.com>
2022-08-09 20:00:17 -0700

    Calulator wokring up until square

--- calculator.py
+++ calculator.py
@@ -5,3 +5,50 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True: #repeat forever
+    calculation_chosen = input("Please type equation here (+ - * / square cube pow mod)") # read input
+    tokens = calculation_chosen.split(' ') # tokenize input
+    num1 = tokens[1]
+    if tokens[2] is True:
+        num2 = tokens[2]
+    # # num3 = tokens[3]
+    
+    if tokens[0] == "q":
+        print("You have quit the calculator, goodbye.")
+        break
+
+    elif tokens[0] == "+":
+        result = add(float(num1), float(num2))
+        
+
+    elif tokens[0] == "-":
+        result = subtract(float(num1), float(num2))
+
+    elif tokens[0] == "*":
+        result = multiply(float(num1), float(num2))
+
+    elif tokens[0] == "/":
+        result = divide(float(num1), float(num2))
+
+    elif tokens[0] == "square":
+        result = square(float(num1))
+
+    elif tokens[0] == "cube":
+        result = cube(float(num1))
+
+    elif tokens[0] == "pow":
+        result = power(float(num1), float(num2))
+
+    elif tokens[0] == "mod":
+        result = mod(float(num1), float(num2))
+
+    # elif tokens[0] == "x+":
+    #     result = add_mult(num1, num2, num3)
+
+    # elif tokens[0] == "cubes+":
+    #     result = add_cubes(num1, num2)
+
+    else:
+        result = "Please enter an operator followed by two integers."
+    
+    print(result)
