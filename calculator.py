6965bf5162d8f131a4c2a18bc00e876c54ce3156
Carina Ogbat <carinawrightogbat@gmail.com>
2022-08-09 21:03:32 -0700

    Checks if digit and for input length haven't tested.

--- calculator.py
+++ calculator.py
@@ -11,10 +11,10 @@ while True: #repeat forever
     
     # # num3 = tokens[3]
 
-    if tokens[1] is True:
+    if len(tokens) > 1:
         num1 = tokens[1]
     
-    if tokens[2] is True:
+    if len(tokens) > 2:
         num2 = tokens[2]
 
     
@@ -23,51 +23,56 @@ while True: #repeat forever
         break
 
     elif tokens[0] == "+":
-        # num1 = tokens[1]
-        # num2 = tokens[2]
         if num1.isdigit() and num2.isdigit():
             result = add(float(num1), float(num2))
         else:
-            print("Sorry, that was not a digit please try again.")
-        
+            result = "Sorry, that was not a digit please try again."
         
 
     elif tokens[0] == "-":
-        num1 = tokens[1]
-        num2 = tokens[2]
-        result = subtract(float(num1), float(num2))
-        
+        if num1.isdigit() and num2.isdigit():
+            result = subtract(float(num1), float(num2))
+        else:
+            result = "Sorry, that was not a digit please try again."
 
     elif tokens[0] == "*":
-        num1 = tokens[1]
-        num2 = tokens[2]
-        result = multiply(float(num1), float(num2))
+        if num1.isdigit() and num2.isdigit():
+            result = multiply(float(num1), float(num2))
+        else:
+            result = "Sorry, that was not a digit please try again."
         
 
     elif tokens[0] == "/":
-        num1 = tokens[1]
-        num2 = tokens[2]
-        result = divide(float(num1), float(num2))
+        if num1.isdigit() and num2.isdigit():
+            result = divide(float(num1), float(num2))
+        else:
+            result = "Sorry, that was not a digit please try again."
         
 
     elif tokens[0] == "square":
-        num1 = tokens[1]
-        result = square(float(num1))
+        if num1.isdigit() and num2.isdigit():
+            result = square(float(num1))
+        else:
+            result = "Sorry, that was not a digit please try again."
 
     elif tokens[0] == "cube":
-        num1 = tokens[1]
-        result = cube(float(num1))
+        if num1.isdigit() and num2.isdigit():
+            result = cube(float(num1))
+        else:
+            result = "Sorry, that was not a digit please try again."
 
     elif tokens[0] == "pow":
-        num1 = tokens[1]
-        num2 = tokens[2]
-        result = power(float(num1), float(num2))
+        if num1.isdigit() and num2.isdigit():
+            result = pow(float(num1), float(num2))
+        else:
+            result = "Sorry, that was not a digit please try again."
         
 
     elif tokens[0] == "mod":
-        num1 = tokens[1]
-        num2 = tokens[2]
-        result = mod(float(num1), float(num2))
+        if num1.isdigit() and num2.isdigit():
+            result = mod(float(num1), float(num2))
+        else:
+            result = "Sorry, that was not a digit please try again."
 
     # elif tokens[0] == "x+":
     #     result = add_mult(num1, num2, num3)
